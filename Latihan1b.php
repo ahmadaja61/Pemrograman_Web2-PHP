b39b15118819ec26936a5153e3ba3fb198ddbfb1
Abang_Huda61 <137385854+ahmadaja61@users.noreply.github.com>
2023-10-28 19:14:35 +0700

    Add files via upload

---
+++ Latihan1b.php
@@ -0,0 +1,22 @@
+<?php
+
+$x = 10;
+
+echo "Aku adalah angka $x</br>";
+
+//Perkalian
+$hasilKali = $x * 8; //Tanda * adalah operator
+echo "Jika Aku di kali 8, jumlahku sekarang $hasilKali </br>";
+
+//Pembagian
+$hasilBagi = $hasilKali / 4; //Tanda / adalah operator
+echo "Jika Aku di bagi 4, jumlahku sekarang $hasilBagi </br>";
+
+//Pengurangan
+$hasilKurang = $hasilBagi - 6; //Tanda / adalah operator
+echo "Jika Aku di kurang 6, jumlahku sekarang $hasilKurang </br>";
+
+//Pertambahan
+$hasilTambah = $hasilKurang + 2; //Tanda / adalah operator
+echo "Jika Aku di tambah 2, jumlahku sekarang $hasilTambah </br>";
+?>
